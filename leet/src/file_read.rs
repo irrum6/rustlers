164a2fd40374ab7b6dcf84db27e8a6d1eeebe92f
irrum6 <irrum6@github.com>
2021-03-13 15:06:22 +0400

    rf2

--- leet/src/file_read.rs
+++ leet/src/file_read.rs
@@ -1,12 +1,42 @@
 pub mod fs1{
+    use crate::leet::leet::TableEntry as TableEntry;
+    use crate::leet::leet::TableOfSubstitions as TableOfSubstitions;
+
     use std::fs::File;
     use std::io::{self, BufRead};
     use std::path::Path;
 
-//copied from rust site, if you don't undestand this don't worry , so did I.
-pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-        let file = File::open(filename)?;
-        Ok(io::BufReader::new(file).lines())
+    //copied from rust site, if you don't undestand this don't worry , so did I.
+    pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+        where P: AsRef<Path>, {
+            let file = File::open(filename)?;
+            Ok(io::BufReader::new(file).lines())
+    }
+
+    /**
+     * receives string literal as file name, opens read, and
+     * then fills tables
+     */
+    pub fn read_file_and_fill(s:&str, tos: &mut TableOfSubstitions){
+        if let Ok(lines) = read_lines(s) {
+            // Consumes the iterator, returns an (Optional) String
+            for line in lines {
+                if let Ok(ip) = line {                
+                    let mut te = TableEntry::new("a");
+                    let mut i = 0;
+                    let chazar = ip.split(",");                
+                    for chaz in chazar{
+                        if i == 0 {                        
+                            te.set_character(chaz);
+                            i +=1;
+                            continue;
+                        }
+                        te.add(chaz);                    
+                        i +=1;
+                    }
+                    tos.add(te);
+                }
+            }
+        }
     }
 }
--- leet/src/main.rs
+++ leet/src/main.rs
@@ -1,35 +1,15 @@
 mod file_read;
-use file_read::fs1::read_lines;
+use file_read::fs1::read_file_and_fill;
 
 mod leet;
 use leet::leet::TableOfSubstitions;
-use leet::leet::TableEntry;
 
 use std::io::stdin;
 
 fn main() {
     // File leet_table.rawtext must exist in current path
     let mut tos = TableOfSubstitions::new();
-    if let Ok(lines) = read_lines("./leet_table.rawtext") {
-        // Consumes the iterator, returns an (Optional) String
-        for line in lines {
-            if let Ok(ip) = line {                
-                let mut te = TableEntry::new("a");
-                let mut i = 0;
-                let chazar = ip.split(",");                
-                for chaz in chazar{
-                    if i == 0 {                        
-                        te.set_character(chaz);
-                        i +=1;
-                        continue;
-                    }
-                    te.add(chaz);                    
-                    i +=1;
-                }
-                tos.add(te);
-            }
-        }
-    }
+    read_file_and_fill("./leet_table.rawtext", &mut tos);
     //tos.print();
     let mut line = String::new();
     let exit = String::from("exit");

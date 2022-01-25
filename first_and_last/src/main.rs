f163bd6863d65ab18762d01c6063b637f549b1a0
irakli mindorashvili <irrum6@github.com>
2022-01-25 17:42:54 +0400

    moar test

--- first_and_last/src/main.rs
+++ first_and_last/src/main.rs
@@ -18,14 +18,16 @@ fn find_first_and_last_positions(arr: &[i32], search: i32) -> (isize, isize) {
 #[test]
 fn test1() {
     let arr = [0, 1, 2, 3, 3, 3, 3, 4, 4, 5, 6];
-    let result1 = find_first_and_last_positions(&arr, 3);
-    assert_eq!(result1.0, 3);
-    let result1 = find_first_and_last_positions(&arr, 4);
-    assert_eq!(result1.0, 7);
-    let result1 = find_first_and_last_positions(&arr, 5);
-    assert_eq!(result1.0, 9);
-    let result1 = find_first_and_last_positions(&arr, 6);
-    assert_eq!(result1.0, 10);
+    let result = find_first_and_last_positions(&arr, 3);
+    assert_eq!(result.0, 3);
+    let result = find_first_and_last_positions(&arr, 4);
+    assert_eq!(result.0, 7);
+    let result = find_first_and_last_positions(&arr, 5);
+    assert_eq!(result.0, 9);
+    let result = find_first_and_last_positions(&arr, 6);
+    assert_eq!(result.0, 10);
+    let result = find_first_and_last_positions(&arr, 8);
+    assert_eq!(result.1, -1);
 }
 
 fn main() {

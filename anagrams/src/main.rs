237b2f5e8d2443274f7515860e03bc4e5da1efba
irakli mindorashvili <irrum6@github.com>
2022-01-25 17:09:49 +0400

    optim

--- anagrams/src/main.rs
+++ anagrams/src/main.rs
@@ -4,8 +4,8 @@ fn is_anagram(s1: &String, s2: &String) -> bool {
         return false;
     }
     //convert unicode codepoint value to unsigned 32bit intger and then sum it with fold
-    let sum1 = s1.chars().map(|e| e as u32).fold(0, |acc, x| acc + x);
-    let sum2 = s2.chars().map(|e| e as u32).fold(0, |acc, x| acc + x);
+    let sum1 = s1.chars().fold(0, |acc, e| acc + e as u32);
+    let sum2 = s2.chars().fold(0, |acc, e| acc + e as u32);
     //compare sums, for anagrams it should be equal
     sum1 == sum2
 }

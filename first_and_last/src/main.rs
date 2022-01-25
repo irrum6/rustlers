d3d05ed7632762bedd337bbc808c1773379d361f
irakli mindorashvili <irrum6@github.com>
2022-01-25 17:41:46 +0400

    test

--- first_and_last/src/main.rs
+++ first_and_last/src/main.rs
@@ -1,5 +1,6 @@
 // we don't need to consume values there
 fn find_first_and_last_positions(arr: &[i32], search: i32) -> (isize, isize) {
+    // In a sorted array
     let mut start = -1;
     let mut last = -1;
     let mut count = -1;
@@ -14,9 +15,20 @@ fn find_first_and_last_positions(arr: &[i32], search: i32) -> (isize, isize) {
     }
     return (start, last);
 }
+#[test]
+fn test1() {
+    let arr = [0, 1, 2, 3, 3, 3, 3, 4, 4, 5, 6];
+    let result1 = find_first_and_last_positions(&arr, 3);
+    assert_eq!(result1.0, 3);
+    let result1 = find_first_and_last_positions(&arr, 4);
+    assert_eq!(result1.0, 7);
+    let result1 = find_first_and_last_positions(&arr, 5);
+    assert_eq!(result1.0, 9);
+    let result1 = find_first_and_last_positions(&arr, 6);
+    assert_eq!(result1.0, 10);
+}
 
 fn main() {
     let arr = [0, 1, 2, 3, 3, 3, 3, 4, 4, 5, 6];
     println!("{:?}", find_first_and_last_positions(&arr, 3));
-    println!("{:?}", find_first_and_last_positions(&arr, 4));
 }
